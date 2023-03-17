17686203e807778f693e0ad9b91ac7934f9cb943
navjotsingh2608 <navjot@argenelec.com>
2023-03-16 21:14:52 -0400

    Initial commit

---
+++ .history/src/App_20230316210111.tsx
@@ -0,0 +1,10 @@
+import { Button, ButtonGroup } from "@chakra-ui/react";
+function App() {
+  return (
+    <div className="App">
+      <Button>Click</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ .history/src/App_20230316210139.tsx
@@ -0,0 +1,10 @@
+import { Button, ButtonGroup } from "@chakra-ui/react";
+function App() {
+  return (
+    <div className="App">
+      <Button colorScheme="blue">Button</Button>
+    </div>
+  );
+}
+
+export default App;
